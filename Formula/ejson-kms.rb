be5d31e7c7c164abf3da3e24d4449fa9e1746382
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2016-09-28 18:11:25 +0200

    Formula for 1.0.0

--- Formula/ejson-kms.rb
+++ Formula/ejson-kms.rb
@@ -1,9 +1,9 @@
 class EjsonKms < Formula
   desc "ejson-kms is a utility for managing a collection of secrets in source control using AWS KMS."
   homepage "https://github.com/adrienkohlbecker/ejson-kms"
-  url "https://github.com/adrienkohlbecker/ejson-kms/archive/1.0.0-rc1.tar.gz"
-  version "1.0.0-rc1"
-  sha256 "ba03547ab6a6c1bcc0f880f3049274deda71e93053fee91ee308f846632a1e92"
+  url "https://github.com/adrienkohlbecker/ejson-kms/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "ad92b3260ff4a738389c56052f59f4e4a5b59232027d00002d35663e103ab0b3"
 
   depends_on "go" => :build
 
