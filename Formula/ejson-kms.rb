1b1e00cf4eb4e269cac6037bf03b3987cc588bc9
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2019-01-29 22:09:30 -0300

    Release v4

--- Formula/ejson-kms.rb
+++ Formula/ejson-kms.rb
@@ -1,9 +1,9 @@
 class EjsonKms < Formula
   desc "ejson-kms is a utility for managing a collection of secrets in source control using AWS KMS."
   homepage "https://github.com/adrienkohlbecker/ejson-kms"
-  url "https://github.com/adrienkohlbecker/ejson-kms/releases/download/3.0.0/ejson-kms-3.0.0.tar.gz"
-  version "3.0.0"
-  sha256 "3688b502d7d74611a8de3ee1cac1979874a56453248e0a4164129abf29187905"
+  url "https://github.com/adrienkohlbecker/ejson-kms/releases/download/4.0.0/ejson-kms-4.0.0.tar.gz"
+  version "4.0.0"
+  sha256 "06486f2e7eb465346c1db2628e55f14cc262c5257a85994864e64a87210b5cd7"
 
   depends_on "go" => :build
 
@@ -12,7 +12,7 @@ class EjsonKms < Formula
     path = buildpath/"src/github.com/adrienkohlbecker/ejson-kms"
     path.install Dir["*"]
     cd path do
-      system "env", "VERSION=3.0.0", "SHA1=17e26365fe77a20237f8cf8bab13708e94229634", "OUTPUT=#{bin}/ejson-kms", "bin/compile"
+      system "env", "VERSION=4.0.0", "SHA1=def6aec1de9bb3ace51c8bef8a4dd06c34cf32b4", "OUTPUT=#{bin}/ejson-kms", "bin/compile"
       man.mkpath
       man1.install Dir["doc/man/*.1"]
     end
